22158f9d9f43b37425e80c38211fe7427696034e
munchieCORBIN <corbin@munchiemeets.com>
2016-04-05 22:27:42 -0700

    Custom TableView Cell rewrite
    04/05/16
    Removed Search.swift, created SearchResults.swift.
    New approach.

---
+++ MMBETA/SearchResults.swift
@@ -0,0 +1,38 @@
+//
+//  SearchResults.swift
+//  MMBETA
+//
+//  Created by Corbin Benally on 4/5/16.
+//  Copyright © 2016 Munchie Meets. All rights reserved.
+//
+import UIKit
+
+class SearchResults: UITableViewController{ 
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell") as UITableViewCell!
+        
+        cell.textLabel?.text = "Corbin TERST"
+        
+        return cell
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        
+    }
+
+
+
+}
+
